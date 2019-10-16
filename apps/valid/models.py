a1cc7df0a0487ad9e93a263849541d7d3361cecd
Chris Hughes <chris@speedycomputing.com>
2019-10-15 22:33:27 -0700

    Initial

---
+++ apps/valid/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+# regex requires at least 1 number, capital letter, lower case letter, permits some special characters !@#$%^&*+= and must be 8 characters or longer
+PASSWORD_REGEX = re.compile(r'^(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])[A-Za-z\d,!@#$%^&*+=]{8,}$')
+
+class UserManager(models.Manager):
+    def register(self, postData):
+        messages = []
+
+        if len(postData['email']) < 1:
+            messages.append('Email is required!')
+        elif not EMAIL_REGEX.match(postData['email']):
+            messages.append('Invalid Email!')
+        else:
+            check = User.objects.filter(email=postData['email'].lower())
+            if len(check) > 0:
+                messages.append('Email already in use!')
+
+        if len(postData['password']) < 1:
+            messages.append('Password is required!')
+        elif not PASSWORD_REGEX.match(postData['password']):
+            messages.append('Password must contain at least 1 number and capitalization!')
+
+        if len(postData['password_confirm']) < 1:
+            messages.append('Confirm password is required!')
+        elif postData['password_confirm'] != postData['password']:
+            messages.append('Password must match Confirm password!')
+
+        return messages
+
+    def login(self, postData):
+        messages = []
+
+        if len(postData['email']) < 1:
+            messages.append('Email is required!')
+
+        if len(postData['password']) < 1:
+            messages.append('Password is required!')
+
+        return messages
+
+class User(models.Model):
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+    description = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+    
+    def __repr__(self):
+        return f"<User object: {self.email}>"
---
+++ apps/valid/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
+
+def home(request):
+    return render(request, "valid/index.html")
+
+def register(request):
+    errors = User.objects.register(request.POST)
+    if errors:
+        for error in errors:
+            messages.error(request, error)
+        return redirect("/")
+    else:
+        pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+        user = User.objects.create(email=request.POST['email'].lower(), password=pw_hash)
+        request.session['user_id'] = user.id
+        return redirect("/success")    
+
+def login(request):
+    errors = User.objects.login(request.POST)
+    if errors:
+        for error in errors:
+            messages.error(request, error)
+        return redirect("/")
+    else:
+        user = User.objects.filter(email=request.POST['email'].lower())
+        if len(user) < 1:
+            messages.error(request, "No User for that email")
+            return redirect("/")
+        
+        if bcrypt.checkpw(request.POST['password'].encode(), user[0].password.encode()):
+            print(f"Setting session value 'user_id' = {user[0].id}")
+            request.session['user_id'] = user[0].id
+            return redirect("/success")
+        else:
+            messages.error(request, "Incorrect Password!")
+            return redirect("/")
+
+def logout(request):
+    request.session.clear()
+    messages.success(request, "Log out successful!")
+    return redirect("/")
+
+def success(request):
+    context = {
+        "user_id" : request.session['user_id']
+    }
+    print('Rendering success page')
+    return render(request, "valid/success.html", context)
